5ce79a7a4fb281f246806728fc54865a7191e148
ivan-pl809 <pon20007@byui.edu>
2025-01-30 23:18:50 -0600

    commit

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GITHUB_CLIENT_ID,
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
-      callbackURL: "http://localhost:5000/auth/github/callback",
+      callbackURL: "https://cse-341-project2-zszp.onrender.com/auth/github/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
